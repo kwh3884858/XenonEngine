6296bc53da8b2956379ba9ba94a5dcbe97c14a61
FREDERICK\whkong <kwh3884858@gmail.com>
2021-04-30 00:59:10 +0800

    Finish DirectX Input. Need input system integrate to engine. Need debug.

--- Windows/Input/DirectXInput.cpp
+++ Windows/Input/DirectXInput.cpp
@@ -23,6 +23,7 @@ namespace WindowInput {
         result = DirectInput8Create(m_hwnd, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&lpdi, nullptr);
         assert(result == DI_OK);
        
+        //Keybroad
         result = lpdi->CreateDevice(GUID_SysKeyboard, &lpdikey, nullptr);
         assert(result == DI_OK);
 
@@ -32,6 +33,7 @@ namespace WindowInput {
         result = lpdikey->SetDataFormat(&c_dfDIKeyboard);
         assert(result == DI_OK);
 
+        //Mouse
         result = lpdi->CreateDevice(GUID_SysMouse, &lpdiMouse, nullptr);
         assert(result == DI_OK);
 
@@ -41,6 +43,24 @@ namespace WindowInput {
         result = lpdiMouse->SetDataFormat(&c_dfDIMouse);
         assert(result == DI_OK);
 
+        result = lpdiMouse->Acquire();
+        assert(result == DI_OK);
+
+        //Joystick
+        result = lpdi->EnumDevices(DI8DEVTYPE_JOYSTICK, DirectInputEnumJoystick, &m_joyStickGUID, DIEDFL_ATTACHEDONLY);
+        assert(result == DI_OK);
+
+        result = lpdi->CreateDevice(m_joyStickGUID, &lpdiJoystick,nullptr);
+        assert(result == DI_OK);
+
+        lpdiJoystick->SetCooperativeLevel(m_hwnd, DISCL_BACKGROUND | DISCL_NONEXCLUSIVE);
+        assert(result == DI_OK);
+
+        lpdiJoystick->SetDataFormat(&c_dfDIJoystick);
+        assert(result == DI_OK);
+
+        result = lpdiJoystick->Acquire();
+        assert(result == DI_OK);
     }
 
     void DirectXInput::Update()
@@ -56,10 +76,36 @@ namespace WindowInput {
                 break;
             }
         }
+
+        lpdiMouse->GetDeviceState(sizeof(m_mouseState), &m_mouseState);
+        assert(result == DI_OK);
+
+        result = lpdiJoystick->Poll();
+        assert(result == DI_OK);
+
+        lpdiJoystick->GetDeviceData(sizeof(m_joystickState), &m_joystickState);
+        assert(result == DI_OK);
+
     }
 
     void DirectXInput::ShutDown()
     {
+        if (lpdiJoystick)
+        {
+            lpdiJoystick->Unacquire();
+        }
+        if (lpdiJoystick)
+        {
+            lpdiJoystick->Release();
+        }
+        if (lpdimouse)
+        {
+            lpdiMouse->Unacquire();
+        }
+        if (lpdiMouse)
+        {
+            lpdiMouse->Release();
+        }
         if (lpdikey)
         {
             lpdikey->Unacquire();
@@ -74,9 +120,26 @@ namespace WindowInput {
         }
     }
 
-    bool DirectXInput::IsKeyDown(unsigned char keyCode) const
+    bool DirectXInput::GetKeyDown(unsigned char keyCode) const
     {
         return m_keyState[keyCode] & 0x80;
+    }
+
+    Vector2f DirectXInput::GetMouseMove() const
+    {
+        return Vector2f(m_mouseState.lX, m_mouseState.lY);
+    }
+
+    bool DirectXInput::GetMouseButton(unsigned char mouseCode) const
+    {
+        return m_mouseState.rgbButtons[mouseCode] & 0x80;
+    }
+
+    bool DirectXInput::DirectInputEnumJoystick(LPCDIDEVICEINSTANCE lpddi, LPVOID guidPointer)
+    {
+        *(GUID*)guidPointer = lpddi->guidInstance;
+        strcpy(m_joyStickName, (char*)lpddi->tszProductName);
+        return (DIENUM_STOP);
     }
 
 }
--- Windows/Input/DirectXInput.h
+++ Windows/Input/DirectXInput.h
@@ -7,12 +7,16 @@
 
 #pragma comment(lib, "dinput.lib")
 #include <dinput.h>
+#include "Mathlab/Vector2f.h"
 
 namespace WindowInput {
+    using MathLab::Vector2f;
 
     class DirectXInputConfig {
         HWND m_hwnd;
     };
+    const unsigned char RightButton = MOUSE_RIGHT_BUTTON;
+    const unsigned char LeftButton = MOUSE_LEFT_BUTTON;
 
     class DirectXInput
     {
@@ -25,14 +29,24 @@ namespace WindowInput {
         void Update();
         void ShutDown();
 
-        bool IsKeyDown(unsigned char keyCode)const;
+        bool GetKeyDown(unsigned char keyCode)const;
+        Vector2f GetMouseMove()const;
+        bool GetMouseButton(unsigned char mouseCode)const;
+
+        bool CALLBACK DirectInputEnumJoystick(LPCDIDEVICEINSTANCE lpddi, LPVOID guidPointer);
+
     private:
         LPDIRECTINPUT8 lpdi = nullptr;
         IDirectInputDevice8 lpdikey = nullptr;
         LPDIRECTINPUTDEVICE8 lpdiMouse = nullptr;
+        LPDIRECTINPUTDEVICE8 lpdiJoystick = nullptr;
         HWND m_hwnd;
         unsigned char m_keyState[256];
         DIMOUSESTATE m_mouseState;
+        DIJOYSTATE m_joystickState;
+        char m_joyStickName[80];
+        GUID m_joyStickGUID;
+
     };
 
 
