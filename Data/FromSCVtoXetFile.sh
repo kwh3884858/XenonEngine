196a2beb5675a0d3a285b08a61c62ae524029688
FREDERICK\whkong <kwh3884858@gmail.com>
2021-06-21 00:30:09 +0800

    Update utility copy tool, add more security check.

--- Data/FromSCVtoXetFile.sh
+++ Data/FromSCVtoXetFile.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-cp XenonScriptAssemblerMachineInstrction.xet.csv XenonScriptAssemblerMachineInstrction.xet
-cp XenonScriptAssemblerDelimiter.xet.csv XenonScriptAssemblerDelimiter.xet
+read -n2 -p "Old Xet will be covored, are you sure? [Y/n]" answer
+case $answer in
+Y | y)
+    cp XenonScriptAssemblerMachineInstrction.xet.csv XenonScriptAssemblerMachineInstrction.xet
+    cp XenonScriptAssemblerDelimiter.xet.csv XenonScriptAssemblerDelimiter.xet
+    echo "Operation Complete"
+    ;;
+N | n)
+    echo "ok, good bye"
+    ;;
+*)
+    echo "error choice"
+    ;;
+esac
+
+exec /bin/bash
--- Data/FromXetToCSVFile.sh
+++ Data/FromXetToCSVFile.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-cp XenonScriptAssemblerMachineInstrction.xet XenonScriptAssemblerMachineInstrction.xet.csv
-cp XenonScriptAssemblerDelimiter.xet XenonScriptAssemblerDelimiter.xet.csv
+read -n2 -p "Old CSV will be covored, are you sure? [Y/n]" answer
+case $answer in
+Y | y)
+    cp XenonScriptAssemblerMachineInstrction.xet XenonScriptAssemblerMachineInstrction.xet.csv
+    cp XenonScriptAssemblerDelimiter.xet XenonScriptAssemblerDelimiter.xet.csv
+    echo "Operation Complete"
+    ;;
+N | n)
+    echo "ok, good bye"
+    ;;
+*)
+    echo "error choice"
+    ;;
+esac
+
+exec /bin/bash
