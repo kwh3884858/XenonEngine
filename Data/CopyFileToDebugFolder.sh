8f0e055d0e4da415e2a9b298172c502c1e5c97ce
FREDERICK\whkong <kwh3884858@gmail.com>
2021-06-21 00:32:16 +0800

    Add utility copy tool for xenon assembler script to debug folder.

---
+++ Data/CopyFileToDebugFolder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# read -n2 -p "Copy xet to debug folder? [Y/n]" answer
+# case $answer in
+# Y | y)
+    cp XenonScriptAssemblerMachineInstrction.xet ../Debug/XenonScriptAssemblerMachineInstrction.xet
+    cp XenonScriptAssemblerDelimiter.xet ../Debug/XenonScriptAssemblerDelimiter.xet
+    cp Main.xea ../Debug/Main.xea
+#     echo "Operation Complete"
+#     ;;
+# N | n)
+#     echo "Ok, Good Bye"
+#     ;;
+# *)
+#     echo "Error Choice"
+#     ;;
+# esac
+
+# exec /bin/bash
