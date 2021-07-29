b4307ca9f4950df7dcd7fcd3c42e1fd161c9f8bc
UBISOFT-ORG\whkong <kwh3884858@126.com>
2021-07-29 21:22:46 +0800

    Add enemy entity.

--- Engine/GameplayMain.cpp
+++ Engine/GameplayMain.cpp
@@ -54,6 +54,7 @@ namespace Gameplay {
     GameObject* player;
     GameObject* ground;
     GameObject* bullet;
+    GameObject* enemy;
     Physics2D* physics2D;
     GameObjectWorld* world;
 
@@ -162,6 +163,42 @@ namespace Gameplay {
             Render2D* render2D = new Render2D(bullet);
             render2D->SetConfig(&render2DConfig);
             bullet->AddComponent(render2D);
+        }
+
+        {
+            enemy = new GameObject("Enemy");
+            Transform2D* transform = new Transform2D(enemy);
+            transform->AddPosition(Vector2f(500, 300));
+            enemy->AddComponent(transform);
+
+            int numOfVertex = 4;
+            Vector2f* heroVertex = new Vector2f[numOfVertex];
+            heroVertex[0] = Vector2f(10, -10);
+            heroVertex[1] = Vector2f(10, 10);
+            heroVertex[2] = Vector2f(-10, 10);
+            heroVertex[3] = Vector2f(-10, -10);
+            Polygon2D* heroPolygon = new Polygon2D(Polygon2D::EState::Enable, CrossPlatform::WHITE, numOfVertex, heroVertex);
+            Render2DConfig render2DConfig;
+            render2DConfig.m_polygon2D = heroPolygon;
+            Render2D* render2D = new Render2D(enemy);
+            render2D->SetConfig(&render2DConfig);
+            enemy->AddComponent(render2D);
+
+            PlayerPersonality* personality = new PlayerPersonality(enemy);
+            enemy->AddComponent(personality);
+
+            Rigidbody2D* const rigid = new Rigidbody2D(enemy, false, 5, 10);
+            enemy->AddComponent(rigid);
+
+            BoxCollider2D* collider = new BoxCollider2D(enemy);
+            BoxCollider2DConfig boxCollider2DConfig;
+            boxCollider2DConfig.m_isTrigger = false;
+            boxCollider2DConfig.m_size = Vector2f(20, 20);
+            collider->SetConfig(&boxCollider2DConfig);
+            enemy->AddComponent(collider);
+
+            physics2D->AddGameObject(enemy);
+            world->AddGameObject(enemy);
         }
         compiler = new XenonCompiler;
         compiler->Initialize();
