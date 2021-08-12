6cade0d02d435f2d84614790af07f38221c6b211
UBISOFT-ORG\whkong <kwh3884858@126.com>
2021-08-12 23:41:39 +0800

    Add Polygon3D.

---
+++ CrossPlatform/Polygon3D.cpp
@@ -0,0 +1,15 @@
+#include "Polygon3D.h"
+namespace CrossPlatform 
+{
+
+	Polygon3D::Polygon3D(SColorRGBA color, int numOfPtr, Polygon3DPointer* vertexPointerList, int numOfVertex, MathLab::Vector2f* vertexList)
+	{
+
+	}
+
+	Polygon3D::~Polygon3D()
+	{
+
+	}
+
+}
