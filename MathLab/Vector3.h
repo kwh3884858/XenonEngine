6be3a8b8c4f6ec3b0029699b1727093c02783bcd
FREDERICK\whkong <kwh3884858@gmail.com>
2021-08-28 16:18:20 +0800

    Fix character error.

--- MathLab/Vector3.h
+++ MathLab/Vector3.h
@@ -2,7 +2,7 @@
 //  MacWindow
 //
 //  Created by whkong on 2021-2-28.
-//  Copyright © 2018 whkong. All rights reserved.
+//  Copyright (c) 2018 whkong. All rights reserved.
 #pragma once
 #include "MathLab/Vector2f.h"
 namespace MathLab {
