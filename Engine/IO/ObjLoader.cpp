90a60133eabbd0363a6d8b25040abdf3beed918f
UBISOFT-ORG\whkong <kwh3884858@126.com>
2021-09-01 11:27:22 +0800

    [Task] Complete OBJ loader.

--- Engine/IO/ObjLoader.cpp
+++ Engine/IO/ObjLoader.cpp
@@ -37,17 +37,16 @@ namespace XenonEngine
 		int numOfVertex = attrib.vertices.size() / 3;
 		Vector3f* verteces = new Vector3f[attrib.vertices.size() / 3];
 		int numOfIndex = 0;
-		int* vertexPointerList = new int[attrib.vertices.size() / 3];
+		int* vertexIndexList = new int[attrib.vertices.size() / 3];
 
-		size_t vindex = 0;
 		for (size_t i = 0; i < attrib.vertices.size(); i+=3)
 		{
-			verteces[vindex].x = attrib.vertices[i + 0];
-			verteces[vindex].y = attrib.vertices[i + 1];
-			verteces[vindex].z = attrib.vertices[i + 2];
-			vindex++;
+			verteces[i / 3].x = attrib.vertices[i + 0];
+			verteces[i / 3].y = attrib.vertices[i + 1];
+			verteces[i / 3].z = attrib.vertices[i + 2];
 		}
-		vindex = 0;
+
+		size_t vindex = 0;
 		// Loop over shapes
 		for (size_t s = 0; s < shapes.size(); s++) 
 		{
@@ -59,51 +58,13 @@ namespace XenonEngine
 				for (size_t v = 0; v < fv; v++) 
 				{
 					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-					vertexPointerList[vindex++] = idx.vertex_index;
+					vertexIndexList[vindex++] = idx.vertex_index;
 				}
 				index_offset += fv;
 			}
 		}
-		// Loop over shapes
-		for (size_t s = 0; s < shapes.size(); s++) {
-			// Loop over faces(polygon)
-			size_t index_offset = 0;
-			for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-				size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
 
-				// Loop over vertices in the face.
-				for (size_t v = 0; v < fv; v++) {
-					// access to vertex
-					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-					tinyobj::real_t vx = attrib.vertices[3 * size_t(idx.vertex_index) + 0];
-					tinyobj::real_t vy = attrib.vertices[3 * size_t(idx.vertex_index) + 1];
-					tinyobj::real_t vz = attrib.vertices[3 * size_t(idx.vertex_index) + 2];
-
-					// Check if `normal_index` is zero or positive. negative = no normal data
-					if (idx.normal_index >= 0) {
-						tinyobj::real_t nx = attrib.normals[3 * size_t(idx.normal_index) + 0];
-						tinyobj::real_t ny = attrib.normals[3 * size_t(idx.normal_index) + 1];
-						tinyobj::real_t nz = attrib.normals[3 * size_t(idx.normal_index) + 2];
-					}
-
-					// Check if `texcoord_index` is zero or positive. negative = no texcoord data
-					if (idx.texcoord_index >= 0) {
-						tinyobj::real_t tx = attrib.texcoords[2 * size_t(idx.texcoord_index) + 0];
-						tinyobj::real_t ty = attrib.texcoords[2 * size_t(idx.texcoord_index) + 1];
-					}
-
-					// Optional: vertex colors
-					// tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
-					// tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
-					// tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+2];
-				}
-				index_offset += fv;
-
-				// per-face material
-				shapes[s].mesh.material_ids[f];
-			}
-		}
-
-		Polygon3D* polygon = new Polygon3D()
+		Polygon3D* polygon = new Polygon3D(numOfVertex, vertexIndexList, numOfVertex, verteces);
+		return polygon;
 	}
 }
