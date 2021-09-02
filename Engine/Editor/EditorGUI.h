dbf989e5c1daf92cd3736182327af61687e484ed
UBISOFT-ORG\whkong <kwh3884858@126.com>
2021-09-02 21:35:18 +0800

    [Task] Add imgui as engine editor framework.

---
+++ Engine/Editor/EditorGUI.h
@@ -0,0 +1,21 @@
+#pragma once
+namespace XenonEditor
+{
+	class EditorGUI
+	{
+	public:
+		EditorGUI();
+		~EditorGUI();
+
+	private:
+
+	};
+
+	EditorGUI::EditorGUI()
+	{
+	}
+
+	EditorGUI::~EditorGUI()
+	{
+	}
+}
