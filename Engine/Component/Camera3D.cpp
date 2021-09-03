eccbe2eba902a798bde85378f3cdef82ccd39b7b
UBISOFT-ORG\whkong <kwh3884858@126.com>
2021-09-03 21:22:30 +0800

    [Refactor] Refactor Camera3D.

---
+++ Engine/Component/Camera3D.cpp
@@ -0,0 +1,13 @@
+#include "Camera3D.h"
+
+namespace XenonEngine
+{
+
+	IComponent* Camera3D::Copy(GameObject*const gameObject) const
+	{
+
+	}
+
+	ComponentType Camera3D::m_type = ComponentType::ComponentType_Camera;
+
+}
---
+++ Engine/Component/Camera3D.h
@@ -0,0 +1,29 @@
+//  Camera3D.h
+//  XenonEngine
+//
+//  Created by whkong on 2021-9-3.
+//  Copyright (c) 2021 whkong. All rights reserved.
+#pragma once
+#include "MathLab/Vector3.h"
+#include "IComponent.h"
+
+namespace XenonEngine
+{
+	class Camera3D :public IComponent
+	{
+	public:
+		Camera3D(GameObject* gameobject) :
+			IComponent(gameobject) {}
+		virtual ~Camera3D()override {}
+		virtual ComponentType GetComponentType() const override { return m_type; };
+		virtual IComponent* Copy(GameObject*const gameObject)const override;
+
+		static ComponentType m_type;
+	private:
+		MathLab::Vector3f m_position;
+		MathLab::Vector3f m_lookAt;
+		MathLab::Vector2f m_viewDistance;
+		float m_fov;
+		MathLab::Vector2f m_viewport;
+	};
+}
