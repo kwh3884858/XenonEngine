8bf74484f41d4f5712042d3e38f63a8206716960
UBISOFT-ORG\whkong <kwh3884858@126.com>
2021-09-03 21:21:31 +0800

    [Refactor] Refactor Matrix3.

--- MathLab/Matrix3.h
+++ MathLab/Matrix3.h
@@ -5,27 +5,66 @@
 //  Copyright © 2018 whkong. All rights reserved.
 
 #pragma once
-
 #include "Vector3.h"
 
 namespace MathLab {
-    struct Matrix3 final{
-        Vector3f v1;
-        Vector3f v2;
-        Vector3f v3;
+	template<typename T>
+	struct Matrix3;
 
+
+	template<typename T>
+    struct Matrix3 final{
+        Vector3<T> v1;
+        Vector3<T> v2;
+        Vector3<T> v3;
 
     public:
         Matrix3();
-        Matrix3(const Vector3f& aV1, const Vector3f& aV2, const Vector3f& aV3);
+        Matrix3(const Vector3<T>& aV1, const Vector3<T>& aV2, const Vector3<T>& aV3);
         Matrix3(float v1, float v2, float v3, float v4, float v5, float v6, float v7, float v8, float v9);
         Matrix3(const Matrix3& aMatrix);
         ~Matrix3();
 
     private:
-
-
     };
+	template<typename T>
+    Vector3<T> operator*(const Vector3<T>& lValue, const Matrix3<T>& rValue);
 
-    Vector3f operator*(const Vector3f& lValue, const Matrix3& rValue);
+	template<typename T>
+	Matrix3<T>::Matrix3()
+	{
+	}
+
+	template<typename T>
+	Matrix3<T>::Matrix3(const Vector3<T>& aV1, const Vector3<T>& aV2, const Vector3<T>& aV3)
+	{
+		v1 = aV1;
+		v2 = aV2;
+		v3 = aV3;
+	}
+
+	template<typename T>
+	Matrix3<T>::Matrix3(const Matrix3& aMatrix)
+	{
+		v1 = aMatrix.v1;
+		v2 = aMatrix.v2;
+		v3 = aMatrix.v3;
+	}
+
+	template<typename T>
+	Matrix3<T>::~Matrix3()
+	{
+
+	}
+
+	template<typename T>
+	Vector3<T> operator*(const Vector3<T>& lValue, const Matrix3<T>& rValue)
+	{
+		Vector3f result;
+		result.x = lValue.x * rValue.v1.x + lValue.y * rValue.v2.x + lValue.z * rValue.v3.x;
+		result.y = lValue.x * rValue.v1.y + lValue.y * rValue.v2.y + lValue.z * rValue.v3.y;
+		result.z = lValue.x * rValue.v1.z + lValue.y * rValue.v2.z + lValue.z * rValue.v3.z;
+		return result;
+	}
+
 }
