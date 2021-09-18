053c8ceaaf74876ae410c0d2233c8351ea771c9b
Kong Weihang <kwh3884858@126.com>
2021-09-18 16:37:17 +0800

    [Refactor] Rename project from MacWindow to XenonEngine. Replace folder to folder reference.

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -1,20 +1,19 @@
 #pragma once
-
+#ifdef _WIN32
 #define  IS_PC_PLATFORM true
-
+#endif
 #if IS_PC_PLATFORM
 #include <tchar.h>
 
 #include "MainWindow.h"
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
-#else
-int main(int args, char* argv[])
-#endif
 {
-	MainWindow window(hInstance);
-	window.Initialize();
-	window.Run();
-	window.Shutdown();
-	return 0;
+    MainWindow window(hInstance);
+    window.Initialize();
+    window.Run();
+    window.Shutdown();
+    return 0;
 }
+
+#endif
