ecdc20c33af171ec70335bf175ebae91e0f35158
Kong Wei Hang <kwh3884858@gmail.com>
2022-08-27 16:41:14 +0800

    Update Render List. Now only polygon3D will be transferred to pipeline.

--- Engine/Graphic/RenderList3D.h
+++ Engine/Graphic/RenderList3D.h
@@ -1,29 +1,20 @@
 #pragma once
 
-#include "MathLab/Vector3.h"
-#include "MathLab/Vector2.h"
+//#include "MathLab/Vector3.h"
+//#include "MathLab/Vector2.h"
 #include "Algorithms/Vector.h"
-#include "CrossPlatform/Polygon/Vertex3D.h"
-#include "CrossPlatform/Material/Material.h"
+//#include "CrossPlatform/Polygon/Vertex3D.h"
+//#include "CrossPlatform/Material/Material.h"
+
+namespace CrossPlatform
+{
+	class Polygon3D;
+}
 
 namespace XenonEngine {
-	using MathLab::Vector3f;
-	using MathLab::Vector2f;
-	using CrossPlatform::Vertex3D;
-	using CrossPlatform::Material;
-	
-	class Mesh3D;
-
-	class RenderElement
-	{
-		Polygon3D* m_polygon;
-		Material*
-	};
-
 	class RenderList3D
 	{
 		void AddMesh3D(const Mesh3D* const mesh);
-		Algorithm::Vector<Polygon3D> m_vertexIndexList;
-		Algorithm::Vector<Material> m_materials;
+		Algorithm::Vector<CrossPlatform::Polygon3D*> m_materials;
 	};
 }
